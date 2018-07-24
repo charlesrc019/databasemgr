7e658ee54db2accfe4de66f27937c5bce0222859
Charles Christensen <30779702+charlesrc19@users.noreply.github.com>
2018-07-24 17:15:54 -0600

    Add files via upload

---
+++ Scripts/custom_viewdatabase.js
@@ -0,0 +1,20 @@
+﻿// Event listeners.
+document.addEventListener('DOMContentLoaded', noPassword);
+
+// Functions.
+function showPassword() {
+    document.getElementById("hiddenPassword").style.display = "inline";
+    document.getElementById("hidePasswordButton").style.display = "inline";
+    document.getElementById("showPasswordButton").style.display = "none";
+}
+
+function hidePassword() {
+    document.getElementById("hiddenPassword").style.display = "none";
+    document.getElementById("hidePasswordButton").style.display = "none";
+    document.getElementById("showPasswordButton").style.display = "inline";
+}
+
+function noPassword() {
+    if (document.getElementById("hiddenPassword").innerText == "")
+        document.getElementById("showPasswordButton").style.display = "none";
+}
---
+++ Scripts/custom_viewserver.js
@@ -0,0 +1,20 @@
+﻿// Event listeners.
+
+// Functions.
+function showDatabases(serverID) {
+    var showID = "ShowDatabases_" + serverID;
+    var hideID = "HideDatabases_" + serverID;
+    var tableID = "Databases_" + serverID;
+    document.getElementById(tableID).style.display = "table";
+    document.getElementById(hideID).style.display = "inline";
+    document.getElementById(showID).style.display = "none";
+}
+
+function hideDatabases(serverID) {
+    var showID = "ShowDatabases_" + serverID;
+    var hideID = "HideDatabases_" + serverID;
+    var tableID = "Databases_" + serverID;
+    document.getElementById(tableID).style.display = "none";
+    document.getElementById(hideID).style.display = "none";
+    document.getElementById(showID).style.display = "inline";
+}
